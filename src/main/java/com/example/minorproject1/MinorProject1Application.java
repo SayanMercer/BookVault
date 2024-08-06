b180798102b624236709e33c55badec49d4302fe
SayanMercer <dassayan14.1998@gmail.com>
2024-08-06 21:50:13 +0530

    All services added

--- src/main/java/com/example/minorproject1/MinorProject1Application.java
+++ src/main/java/com/example/minorproject1/MinorProject1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MinorProject1Application {
+public class MinorProject1Application{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinorProject1Application.class, args);
